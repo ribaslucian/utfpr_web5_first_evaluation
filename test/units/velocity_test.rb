4e2e3155ca17fea08f3add07638263109f22ca7e
ribaslucian <ribas.lucian@gmail.com>
2016-09-20 20:09:23 -0300

    Criando testes unitários.

--- test/units/velocity_test.rb
+++ test/units/velocity_test.rb
@@ -1,8 +1,25 @@
 require_relative '../test_helper'
+require_relative '../../lib/unit/velocity'
 
 # docs
 class VelocityTest < Minitest::Test
-  def test_unit_velocity_default
-    assert_equal 1, 1
+  def test_meter_to_meter
+    v = Velocity.new
+    assert_equal 1, v.meter_to_meter(1)
+  end
+
+  def test_kilometer_to_kilometer
+    v = Velocity.new
+    assert_equal 1, v.kilometer_to_kilometer(1)
+  end
+
+  def test_mile_to_mile
+    v = Velocity.new
+    assert_equal 1, v.mile_to_mile(1)
+  end
+
+  def test_knotted_to_knotted
+    v = Velocity.new
+    assert_equal 1, v.knotted_to_knotted(1)
   end
 end
--- test/units/weight_test.rb
+++ test/units/weight_test.rb
@@ -1,8 +1,35 @@
 require_relative '../test_helper'
+require_relative '../../lib/unit/weight'
 
 # docs
 class WeightTest < Minitest::Test
-  def test_unit_weight_default
-    assert_equal 1, 1
+  def test_grass_to_grass
+    v = Weight.new
+    assert_equal 1, v.grass_to_grass(1)
+  end
+
+  def test_tonne_to_tonne
+    v = Weight.new
+    assert_equal 1, v.tonne_to_tonne(1)
+  end
+
+  def test_lb_to_lb
+    v = Weight.new
+    assert_equal 1, v.lb_to_lb(1)
+  end
+
+  def test_jaguar_to_jaguar
+    v = Weight.new
+    assert_equal 1, v.jaguar_to_jaguar(1)
+  end
+
+  def test_kilogram_to_kilogram
+    v = Weight.new
+    assert_equal 1, v.kilogram_to_kilogram(1)
+  end
+
+  def test_quilate_to_quilate
+    v = Weight.new
+    assert_equal 1, v.quilate_to_quilate(1)
   end
 end
